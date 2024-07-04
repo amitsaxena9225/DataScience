8983cc5341e0d4af7312f89a4fc2b09ab01db5b0
AMIT SAXENA <amitsaxena9225@gmail.com>
2024-07-04 22:08:16 +0530

    Test

---
+++ Numpy/test.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# create numpy array from python list
+
+my_list = [1,2,3,4]
+
+np.array(my_list)
+nes_lst = [[1,2,3],[4,5,6],[7,8,0]]
+
+np.array(nes_lst)
+# There are lot of ways to create arrays
+# Build in Method
+
+# 1. arange() method:
+
+print(np.arange(1,10))
